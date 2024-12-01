70446997300d854fa0940269e0ac0dd068275ea8
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-12-01 09:50:15 -0300

    chore: start using shadcn card component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,5 +1,11 @@
-import clsx from "clsx";
 import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { initialTickets } from "@/data";
 import { ticketPath } from "@/paths";
 
@@ -76,23 +82,29 @@ const TicketsPage = () => {
 
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-from-top">
         {initialTickets.map((ticket) => (
-          <div
-            key={ticket.id}
-            className="w-full max-w-[420px] p-4 border border-slate-100 rounded"
-          >
-            <div>{TICKET_ICONS[ticket.status]}</div>
-            <h3 className="text-lg font-semibold truncate">{ticket.title}</h3>
-            <p
-              className={clsx("text-sm text-slate-500 truncate", {
-                "line-through": ticket.status === "DONE",
-              })}
-            >
-              {ticket.content}
-            </p>
-            <Link href={ticketPath(ticket.id)} className="text-sm underline">
-              View
-            </Link>
-          </div>
+          <Card key={ticket.id} className="w-full max-w-[420px]">
+            <CardHeader>
+              {" "}
+              <CardTitle>
+                <span>{TICKET_ICONS[ticket.status]}</span>
+                <span className="text-lg font-semibold truncate">
+                  {ticket.title}
+                </span>
+              </CardTitle>
+            </CardHeader>
+
+            <CardContent>
+              <p className="line-clamp-3 whitespace-break-spaces">
+                {ticket.content}
+              </p>
+            </CardContent>
+
+            <CardFooter>
+              <Link href={ticketPath(ticket.id)} className="text-sm underline">
+                View
+              </Link>
+            </CardFooter>
+          </Card>
         ))}
       </div>
     </div>
