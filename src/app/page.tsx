ddcbe23b48f44394e8e2735239d1e93e86eb3add
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-12-01 09:03:18 -0300

    chore: finish up to "conditional styles" lecture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
-export default function HomePage() {
-  return <h2 className="text-6xl">Home page</h2>;
-}
+import Link from "next/link";
+import { ticketsPath } from "@/paths";
+
+const HomePage = () => {
+  return (
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">Home</h2>
+        <p className="text-sm text-muted-foreground">
+          Your home place to start
+        </p>
+      </div>
+
+      <div className="flex-1 flex flex-col items-center">
+        <Link href={ticketsPath()} className="text-sm underline">
+          Go to Tickets
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,3 +1,47 @@
-export default function TicketsPage() {
-  return <h2>Tickets page</h2>;
-}
+import clsx from "clsx";
+import Link from "next/link";
+import { initialTickets } from "@/data";
+import { ticketPath } from "@/paths";
+
+const TICKET_ICONS = {
+  OPEN: "O",
+  IN_PROGRESS: ">",
+  DONE: "X",
+};
+
+const TicketsPage = () => {
+  return (
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">Tickets</h2>
+        <p className="text-sm text-muted-foreground">
+          All your tickets at one place
+        </p>
+      </div>
+
+      <div className="flex-1 flex flex-col items-center gap-y-4">
+        {initialTickets.map((ticket) => (
+          <div
+            key={ticket.id}
+            className="w-full max-w-[420px] p-4 border border-slate-100 rounded"
+          >
+            <div>{TICKET_ICONS[ticket.status]}</div>
+            <h3 className="text-lg font-semibold truncate">{ticket.title}</h3>
+            <p
+              className={clsx("text-sm text-slate-500 truncate", {
+                "line-through": ticket.status === "DONE",
+              })}
+            >
+              {ticket.content}
+            </p>
+            <Link href={ticketPath(ticket.id)} className="text-sm underline">
+              View
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TicketsPage;
