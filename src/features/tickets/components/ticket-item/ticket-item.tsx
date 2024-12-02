35dcabd6994ba4ef47bd758fb0c3ba87014a116c
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-12-02 20:57:27 -0300

    refator: move ticket details link button to a const inside the component

--- src/features/tickets/components/ticket-item/ticket-item.tsx
+++ src/features/tickets/components/ticket-item/ticket-item.tsx
@@ -1,11 +1,7 @@
+import { LucideSquareArrowUpRight } from "lucide-react";
 import Link from "next/link";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { TICKET_ICONS } from "@/features/tickets/constants";
 import { type Ticket } from "@/features/tickets/types";
 import { ticketPath } from "@/paths";
@@ -15,24 +11,33 @@ type TicketItemProps = {
 };
 
 export const TicketItem = ({ ticket }: TicketItemProps) => {
+  const detailButton = (
+    <Button variant={"outline"} size={"icon"} asChild>
+      <Link href={ticketPath(ticket.id)}>
+        <LucideSquareArrowUpRight />
+      </Link>
+    </Button>
+  );
   return (
-    <Card className="w-full max-w-[420px]">
-      <CardHeader>
-        <CardTitle>
-          <span>{TICKET_ICONS[ticket.status]}</span>
-          <span className="truncate text-lg font-semibold">{ticket.title}</span>
-        </CardTitle>
-      </CardHeader>
+    <div className="flex w-full max-w-[420px] gap-1">
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle>
+            <span>{TICKET_ICONS[ticket.status]}</span>
+            <span className="truncate text-lg font-semibold">
+              {ticket.title}
+            </span>
+          </CardTitle>
+        </CardHeader>
 
-      <CardContent>
-        <p className="line-clamp-3 whitespace-break-spaces">{ticket.content}</p>
-      </CardContent>
+        <CardContent>
+          <p className="line-clamp-3 whitespace-break-spaces">
+            {ticket.content}
+          </p>
+        </CardContent>
+      </Card>
 
-      <CardFooter>
-        <Link href={ticketPath(ticket.id)} className="text-sm underline">
-          View
-        </Link>
-      </CardFooter>
-    </Card>
+      <div className="flex flex-col gap-y-1">{detailButton}</div>
+    </div>
   );
 };
