e1f77f67979fd8ba915d95a7fdbeac0dfff7d293
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-12-03 21:43:37 -0300

    feat: implement theme switching

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,26 +2,22 @@ import { LucideKanban } from "lucide-react";
 import Link from "next/link";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { homePath, ticketsPath } from "@/paths";
+import { ThemeSwitcher } from "../theme/theme-switcher";
 
 export const Header = () => {
   return (
-    <nav
-      className="
-            supports-backdrop-blur:bg-background/60
-            fixed left-0 right-0 top-0 z-20
-            border-b bg-background/95 backdrop-blur
-            w-full flex py-2.5 px-5 justify-between
-          "
-    >
-      <div>
+    <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 flex w-full justify-between border-b bg-background/95 px-5 py-2.5 backdrop-blur">
+      <div className="flex items-center gap-x-2">
         <Button asChild variant={"ghost"}>
           <Link href={homePath()}>
             <LucideKanban />
-            <h1 className="text-lg font-semibold ml-2">TicketBounty</h1>
+            <h1 className="ml-2 text-lg font-semibold">TicketBounty</h1>
           </Link>
         </Button>
       </div>
-      <div>
+
+      <div className="flex items-center gap-x-2">
+        <ThemeSwitcher />
         <Link
           href={ticketsPath()}
           className={buttonVariants({ variant: "default" })}
