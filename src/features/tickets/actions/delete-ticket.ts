871da6a5a0a4a78a3c566749e47341eefe53e0bc
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-12-29 10:44:06 -0300

    feat: create server action for deleting tickets

---
+++ src/features/tickets/actions/delete-ticket.ts
@@ -0,0 +1,6 @@
+"use server";
+import { prisma } from "@/lib/prisma";
+
+export const deleteTicket = async (ticketId: string) => {
+  await prisma.ticket.delete({ where: { id: ticketId } });
+};
--- src/features/tickets/components/ticket-item/ticket-item.tsx
+++ src/features/tickets/components/ticket-item/ticket-item.tsx
@@ -1,11 +1,49 @@
+"use client";
+
 import { Ticket } from "@prisma/client";
 import clsx from "clsx";
-import { LucideSquareArrowUpRight } from "lucide-react";
+import { LucideSquareArrowUpRight, LucideTrash } from "lucide-react";
 import Link from "next/link";
+import { ComponentProps } from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { TICKET_ICONS } from "@/features/tickets/constants";
+import { TicketId } from "@/features/tickets/types";
 import { ticketPath } from "@/paths";
+import { deleteTicket } from "../../actions/delete-ticket";
+
+type DeleteButtonProps = ComponentProps<typeof Button> & {
+  ticketId: TicketId;
+};
+
+const DeleteButton = ({ ticketId, ...props }: DeleteButtonProps) => {
+  const handleDeleteTicket = () => deleteTicket(ticketId);
+
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={handleDeleteTicket}
+      {...props}
+    >
+      <LucideTrash className="h-4 w-4" />
+    </Button>
+  );
+};
+
+type DetailButtonProps = {
+  ticketId: TicketId;
+};
+
+const DetailButton = ({ ticketId }: DetailButtonProps) => {
+  return (
+    <Button variant={"outline"} size={"icon"} asChild>
+      <Link href={ticketPath(ticketId)}>
+        <LucideSquareArrowUpRight />
+      </Link>
+    </Button>
+  );
+};
 
 type TicketItemProps = {
   ticket: Ticket;
@@ -13,14 +51,6 @@ type TicketItemProps = {
 };
 
 export const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
-  const detailButton = (
-    <Button variant={"outline"} size={"icon"} asChild>
-      <Link href={ticketPath(ticket.id)}>
-        <LucideSquareArrowUpRight />
-      </Link>
-    </Button>
-  );
-
   return (
     <div
       className={clsx("flex w-full gap-x-1", {
@@ -49,9 +79,13 @@ export const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
         </CardContent>
       </Card>
 
-      {isDetail ? null : (
-        <div className="flex flex-col gap-y-1">{detailButton}</div>
-      )}
+      <div className="flex flex-col gap-y-1">
+        {isDetail ? (
+          <DeleteButton ticketId={ticket.id} />
+        ) : (
+          <DetailButton ticketId={ticket.id} />
+        )}
+      </div>
     </div>
   );
 };
