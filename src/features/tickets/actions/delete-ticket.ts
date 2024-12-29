f3fbeb915fd50245e4e19d969bcdd34c021861f2
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-12-29 11:06:57 -0300

    feat: implement redirection to ticket list after deleting a ticket

--- src/features/tickets/actions/delete-ticket.ts
+++ src/features/tickets/actions/delete-ticket.ts
@@ -1,6 +1,9 @@
 "use server";
+import { redirect } from "next/navigation";
 import { prisma } from "@/lib/prisma";
+import { ticketsPath } from "@/paths";
 
 export const deleteTicket = async (ticketId: string) => {
   await prisma.ticket.delete({ where: { id: ticketId } });
+  redirect(ticketsPath());
 };
