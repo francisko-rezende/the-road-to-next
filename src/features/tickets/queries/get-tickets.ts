88ae802682602675d4940e2bb2434b4a9ba5cd32
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-12-28 22:05:24 -0300

    feat: integrate getTicket and getTickets with the supabase db

--- src/features/tickets/queries/get-tickets.ts
+++ src/features/tickets/queries/get-tickets.ts
@@ -1,10 +1,10 @@
-import { initialTickets } from "@/data";
+import { prisma } from "@/lib/prisma";
 import { Ticket } from "../types";
 
 export const getTickets = async (): Promise<Ticket[]> => {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-
-  return new Promise((resolve) => {
-    resolve(initialTickets);
+  return await prisma.ticket.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
   });
 };
