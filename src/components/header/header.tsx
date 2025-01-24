c1900633fd8df55f385e0e9ad9e36335714ba523
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-01-24 19:45:40 -0300

    feat(authentication): add new auth related routes

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,32 @@
 import { LucideKanban } from "lucide-react";
 import Link from "next/link";
 import { Button, buttonVariants } from "@/components/ui/button";
-import { homePath, ticketsPath } from "@/paths";
+import { homePath, signInPath, signUpPath, ticketsPath } from "@/paths";
 import { ThemeSwitcher } from "../theme/theme-switcher";
 
 export const Header = () => {
+  const navItems = (
+    <>
+      <Link
+        href={ticketsPath()}
+        className={buttonVariants({ variant: "default" })}
+      >
+        Tickets
+      </Link>
+      <Link
+        href={signUpPath()}
+        className={buttonVariants({ variant: "outline" })}
+      >
+        Sign up
+      </Link>
+      <Link
+        href={signInPath()}
+        className={buttonVariants({ variant: "outline" })}
+      >
+        Sign in
+      </Link>
+    </>
+  );
   return (
     <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 flex w-full justify-between border-b bg-background/95 px-5 py-2.5 backdrop-blur">
       <div className="flex items-center gap-x-2">
@@ -18,12 +40,7 @@ export const Header = () => {
 
       <div className="flex items-center gap-x-2">
         <ThemeSwitcher />
-        <Link
-          href={ticketsPath()}
-          className={buttonVariants({ variant: "default" })}
-        >
-          Tickets
-        </Link>
+        {navItems}
       </div>
     </nav>
   );
