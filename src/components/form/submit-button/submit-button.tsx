2095145b85a915438f17f6a6259978326a7af1ac
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-01-26 18:48:10 -0300

    feat: add icon, size and variant props to SubmitButton

--- src/components/form/submit-button/submit-button.tsx
+++ src/components/form/submit-button/submit-button.tsx
@@ -1,19 +1,31 @@
 "use client";
 
 import { LucideLoaderCircle } from "lucide-react";
-import { ComponentProps } from "react";
+import { ComponentProps, ReactElement } from "react";
 import { useFormStatus } from "react-dom";
 import { Button } from "@/components/ui/button";
 
-type SubmitButtonProps = ComponentProps<"button">;
+type SubmitButtonProps = ComponentProps<"button"> & {
+  icon?: ReactElement;
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link";
+  size?: "default" | "sm" | "lg" | "icon";
+};
 
-export const SubmitButton = (props: SubmitButtonProps) => {
+export const SubmitButton = ({ icon, ...props }: SubmitButtonProps) => {
   const { pending } = useFormStatus();
+  const shouldRenderIcon = !!icon && !pending;
 
   return (
-    <Button type="submit" disabled={pending}>
+    <Button type="submit" disabled={pending} {...props}>
       {pending && <LucideLoaderCircle className="h-4 w-4 animate-spin" />}
       {props.children}
+      {shouldRenderIcon && icon}
     </Button>
   );
 };
