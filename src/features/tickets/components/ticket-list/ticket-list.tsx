c8b2b77295a06860cb18ba42aac4e40d55915925
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-02-02 18:37:33 -0300

    feat(search & sort): create search-input

--- src/features/tickets/components/ticket-list/ticket-list.tsx
+++ src/features/tickets/components/ticket-list/ticket-list.tsx
@@ -1,3 +1,5 @@
+import { SearchInput } from "@/components/search-input/search-input";
+import { Input } from "@/components/ui/input";
 import { getTickets } from "@/features/tickets/queries/get-tickets";
 import { TicketItem } from "../ticket-item";
 
@@ -9,6 +11,9 @@ export const TicketList = async ({ userId }: TicketListProps) => {
   const tickets = await getTickets({ userId });
   return (
     <div className="flex flex-1 animate-fade-in-from-top flex-col items-center gap-y-4">
+      <div className="w-full max-w-[420px]">
+        <SearchInput placeholder="Search tickets..." />
+      </div>
       {tickets.map((ticket) => (
         <TicketItem key={ticket.id} ticket={ticket} />
       ))}
