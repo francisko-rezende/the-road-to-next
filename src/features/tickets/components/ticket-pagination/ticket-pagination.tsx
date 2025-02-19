3d840c26dd3b24e982dbea063fa88695db33e388
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-02-18 21:31:09 -0300

    feat(pagination): reactively resets the page number when the user searches for something

--- src/features/tickets/components/ticket-pagination/ticket-pagination.tsx
+++ src/features/tickets/components/ticket-pagination/ticket-pagination.tsx
@@ -1,8 +1,13 @@
 "use client";
 
-import { useQueryStates } from "nuqs";
+import { useQueryState, useQueryStates } from "nuqs";
+import { useEffect, useRef } from "react";
 import { Pagination } from "@/components/pagination";
-import { paginationOptions, paginationParser } from "../../search-params";
+import {
+  paginationOptions,
+  paginationParser,
+  searchParser,
+} from "../../search-params";
 
 type TicketPaginationProps = {
   paginatedTicketMetadata: {
@@ -18,6 +23,18 @@ export const TicketPagination = ({
     paginationParser,
     paginationOptions,
   );
+
+  const [search] = useQueryState("search", searchParser);
+  const previousSearch = useRef(search);
+
+  useEffect(() => {
+    if (search === previousSearch.current) return;
+    previousSearch.current = search;
+
+    setPagination({ ...pagination, page: 0 });
+
+    setPagination({ ...pagination, page: 0 });
+  }, [pagination, search, setPagination]);
 
   return (
     <Pagination
