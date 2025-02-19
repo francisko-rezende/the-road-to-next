c64b4b6f4f9eb57772a3b4b17febbcb96573031a
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-02-19 19:55:34 -0300

    feat(route handlers): use search params in the tickets route handler

--- src/app/api/tickets/route.ts
+++ src/app/api/tickets/route.ts
@@ -1,6 +1,13 @@
 import { getTickets } from "@/features/tickets/queries/get-tickets";
+import { searchParamsCache } from "@/features/tickets/search-params";
 
-export async function GET() {
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const untypedSearchParams = Object.fromEntries(searchParams);
+
+  const typedSearchParams = searchParamsCache.parse(untypedSearchParams);
+  console.log(typedSearchParams);
+
   const { list, metada } = await getTickets({
     userId: undefined,
     searchParams: {
