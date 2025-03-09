492a0658688d0a4a3e7d7dabc51f0aeabfadffb9
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-03-09 16:02:57 -0300

    feat(react query): setup react query provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Header } from "@/app/_navigation/header";
 import { Sidebar } from "@/app/_navigation/sidebar/components/sidebar/sidebar";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { ReactQueryProvider } from "./_providers/react-query/react-query-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -35,14 +36,16 @@ export default function RootLayout({
       >
         <NuqsAdapter>
           <ThemeProvider>
-            <Header />
-            <div className="flex h-screen border-collapse overflow-hidden">
-              <Sidebar />
-              <main className="flex min-h-screen flex-1 flex-col overflow-y-auto overflow-x-hidden bg-secondary/20 px-8 py-24">
-                {children}
-              </main>
-            </div>
-            <Toaster expand />
+            <ReactQueryProvider>
+              <Header />
+              <div className="flex h-screen border-collapse overflow-hidden">
+                <Sidebar />
+                <main className="flex min-h-screen flex-1 flex-col overflow-y-auto overflow-x-hidden bg-secondary/20 px-8 py-24">
+                  {children}
+                </main>
+              </div>
+              <Toaster expand />
+            </ReactQueryProvider>
           </ThemeProvider>
         </NuqsAdapter>
       </body>
