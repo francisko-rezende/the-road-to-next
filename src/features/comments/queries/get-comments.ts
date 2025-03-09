8701498173271dc13383f34c6c63cee31da6b81b
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-03-09 11:02:55 -0300

    feat(cursor-based pagination): implement initial version of cursor-based pagination using timestamps

--- src/features/comments/queries/get-comments.ts
+++ src/features/comments/queries/get-comments.ts
@@ -3,18 +3,19 @@ import { getAuth } from "@/features/auth/actions/get-auth";
 import { isOwner } from "@/features/auth/utils/is-owner";
 import { prisma } from "@/lib/prisma";
 
-export const getComments = async (ticketId: string, offset?: number) => {
-  const skip = offset ?? 0;
+export const getComments = async (ticketId: string, cursor?: number) => {
   const take = 2;
   const where = {
     ticketId,
+    createdAt: {
+      lt: cursor ? new Date(cursor) : new Date(),
+    },
   };
 
   const { user } = await getAuth();
   const [comments, count] = await prisma.$transaction([
     prisma.comment.findMany({
       where,
-      skip,
       take,
       include: {
         user: {
@@ -41,7 +42,8 @@ export const getComments = async (ticketId: string, offset?: number) => {
     }),
     metadata: {
       count,
-      hasNextPage: count > skip + take,
+      hasNextPage: true, //todo
+      cursor: comments.at(-1)?.createdAt.valueOf(),
     },
   };
 };
